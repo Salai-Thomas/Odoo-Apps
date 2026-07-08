710d4aca3a2505ceba2447143afa44efaaef6ee0
Salai Thomas <salaithomas582@gmail.com>
2026-07-08 19:52:57 +0630

    purchase_create_bill

---
+++ pruchase_create_bill/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Purchase Create Bill",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Long description of module's purpose
+    """,
+
+    'author': "Salai Thomas",
+    'website': "https://www.linkedin.com/in/salai-thomas-6a7201267",
+
+    'category': 'Uncategorized',
+    'version': '19.0.1.0.0',
+
+    'depends': ['base','purchase'],
+
+    'data': [
+        'views/purchase_order_form_view_inherit.xml'
+    ],
+}
+
