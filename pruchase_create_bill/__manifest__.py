171d226c4567c3cb5518668495d29b0e91e6d4ad
SalaiThomas <salaithomas582@gmail.com>
2026-07-10 16:09:55 +0630

    purchase_create_bill update

--- pruchase_create_bill/__manifest__.py
+++ pruchase_create_bill/__manifest__.py
@@ -1,22 +1,28 @@
 {
     'name': "Purchase Create Bill",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Restores the Create Bill button on purchase orders for simplified invoicing.",
 
     'description': """
-Long description of module's purpose
-    """,
+    This module reintroduces the 'Create Bill' button directly onto the Purchase Order form, 
+    allowing users to quickly generate vendor bills from their purchases, mirroring 
+    workflows from previous Odoo versions.
+        """,
 
     'author': "Salai Thomas",
     'website': "https://www.linkedin.com/in/salai-thomas-6a7201267",
-
-    'category': 'Uncategorized',
+    'category': 'Purchases',
     'version': '19.0.1.0.0',
 
-    'depends': ['base','purchase'],
+    'depends': ['purchase'],
 
     'data': [
         'views/purchase_order_form_view_inherit.xml'
     ],
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
 
